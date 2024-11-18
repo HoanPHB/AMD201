4a823ceeb55a1a4f6be3f68351a588698e900484
NeekOwO <viethailongvuong@gmail.com>
2024-11-18 23:22:40 +0700

    (UNFINISHED) migrate database context to MySQL and update connection settings

---
+++ src/Backend/URLShortenerBackend/Entities/Url.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace URLShortenerBackend.Entities;
+
+public partial class Url
+{
+    public int Id { get; set; }
+
+    public string OriginalUrl { get; set; } = null!;
+
+    public string ShortCode { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime ExpiresAt { get; set; }
+}
